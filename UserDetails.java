c2ff1d20e73f779b61745bb6e0821a016762e962
Amit Khachane <36502730+kimpossible7@users.noreply.github.com>
2018-10-31 09:25:33 +0530

    Add files via upload

---
+++ UserDetails.java
@@ -0,0 +1,15 @@
+package com.example.android.pokedr;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Amit Khachane.
+ */
+public class UserDetails extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
